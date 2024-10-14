485495b4d4093bbd874915975ee5f5125a3515b7
riyasharma2002 <riyasharmar888@gmail.com>
2024-10-14 14:59:28 +0530

    push

--- api/server.js
+++ api/server.js
@@ -18,9 +18,10 @@ app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Static route for image access
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
+
 
 
 const userSchema = new mongoose.Schema({
